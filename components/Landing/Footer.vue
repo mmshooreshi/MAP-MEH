7a916f2b3924c4488dc9599b5b67026872a60a30
Mam Thenebo <mmshooreshi@gmail.com>
2023-08-29 12:45:02 +0330

    new

--- components/Landing/Footer.vue
+++ components/Landing/Footer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-black/50 p-4"></div>
+  <div class="bg-black/50 p-4 font-peyda text-teal"></div>
   <NuxtLink to="http://tdac.iust.ac.ir"> مرکز تحلیل داده‌های حمل‌ونقل</NuxtLink>
 </template>
 
